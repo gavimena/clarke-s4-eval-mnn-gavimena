36006da02620e7d5b77c06fa110ed627517a5715
Gabi Mena <gabriela.grojas@gmail.com>
2018-01-30 19:59:39 +0100

    filter done

--- src/App.js
+++ src/App.js
@@ -21,29 +21,42 @@ class App extends Component {
 		});
 	}
 
-	printCharacters() {
-		let charactersToShow = this.state.characters;
-		return (<ul className="listCharacter">
-      {
-      	charactersToShow.map(
-          character => <li>
-            <HarryPoterList name={character.name} photo={character.image} house={character.house}
-				alive={character.alive? 'live': 'dead'} />
-          </li>
-        )
-      }
-    </ul>);
+	// printCharacters() {
+	// 	let charactersToShow = this.state.characters;
+	// 	return (<ul className="listCharacter">
+   //    {
+   //    	charactersToShow.map(
+   //        character => <li>
+   //          <HarryPoterList name={character.name} photo={character.image} house={character.house}
+	// 			alive={character.alive? 'live': 'dead'} />
+   //        </li>
+   //      )
+   //    }
+   //  </ul>);
+	// }
+
+	filter(e){
+		this.setState({filter: e.target.value})
+		console.log(e);
 	}
 
+
   	render() {
+		let characters = this.state.characters;
+		if(this.state.filter){
+			characters = characters.filter( character => character.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+		}
     return (
       <div className="App">
         <header className="App-header">
          	<h1 className="App-title">Harry Potter Characters</h1>
         </header>
 		  <main>
-		  		<input type="text" name="search"></input>
-				<div>{this.printCharacters()}</div>
+		  		<input type="text" name="search" placeholder="Write a Harry Potter character" onChange={this.filter.bind(this)}/>
+				<div>
+					{characters.map(character => <h2>{character.name}</h2>
+					)}
+				</div>
 		  </main>
       </div>
     );
@@ -51,3 +64,6 @@ class App extends Component {
 }
 
 export default App;
+
+
+// <div>{this.printCharacters()}</div>
